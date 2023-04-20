e637bde5d6d7468d56c91cd7f375a58476e881d9
mina hany <mina.hany460@gmail.com>
2023-04-20 03:53:29 +0200

    connect to mongoDB

--- src/config/db/index.ts
+++ src/config/db/index.ts
@@ -1,12 +1,9 @@
+import mongoose from 'mongoose';
 import config from '../../../config';
 
-const authConnectOptions = {
-  type: "mongodb",
-  url: "mongodb://localhost/db1"
-};
-
+const MONGODB_URI = `${config.DB_TYPE}://${config.DB_HOST}/${config.DB_INSTANCE}`;
 
 export const initDB = async () => {
-
+    // Connect to MongoDB using Mongoose
+    return await mongoose.connect(MONGODB_URI);
 };
-
